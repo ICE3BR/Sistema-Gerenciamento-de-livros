56d7a13cd337c37ac66ee450a5a916f9544d6e06
GustavoQ <gustavoaquinup@gmail.com>
2024-10-12 02:00:54 -0300

    Adicionado decoradores e geradores

--- Sistema_livraria.py
+++ Sistema_livraria.py
@@ -1,6 +1,5 @@
-
 # Adicione a classe Livro
-class livro():
+class Livro():
     def __init__(self, nome, genero, autor_nome, ano, valor):
         self.nome = nome
         self.genero = genero
@@ -12,43 +11,42 @@ class livro():
 _biblioteca_livros = []
 
 # Adicionando livros à lista
-_biblioteca_livros.append(livro("O Senhor dos Anéis", "Fantasia", "J.R.R. Tolkien", 1954, 120.00))
-_biblioteca_livros.append(livro("1984", "Distopia", "George Orwell", 1949, 45.00))
-_biblioteca_livros.append(livro("O Pequeno Príncipe", "Infantil", "Antoine de Saint-Exupéry", 1943, 30.00))
-_biblioteca_livros.append(livro("Dom Casmurro", "Romance", "Machado de Assis", 1899, 25.00))
+_biblioteca_livros.append(Livro("O Senhor dos Anéis", "Fantasia", "J.R.R. Tolkien", 1954, 120.00))
+_biblioteca_livros.append(Livro("1984", "Distopia", "George Orwell", 1949, 45.00))
+_biblioteca_livros.append(Livro("O Pequeno Príncipe", "Infantil", "Antoine de Saint-Exupéry", 1943, 30.00))
+_biblioteca_livros.append(Livro("Dom Casmurro", "Romance", "Machado de Assis", 1899, 25.00))
 
+# Decorador para logar operações realizadas
+def log_operacao(func):
+    def wrapper(*args, **kwargs):
+        print(f"[LOG] Executando: {func.__name__}")
+        resultado = func(*args, **kwargs)
+        return resultado
+    return wrapper
+
+# Modificação da função adicionar_livros com decorador
+@log_operacao
 def adicionar_livros(nome, genero, autor_nome, ano, valor):
-    if adicionar_livros is None:
+    if not nome or not genero or not autor_nome or not ano or not valor:
         print(f"Dados inválidos, tente novamente")
         return False
     else:
-        _biblioteca_livros.append(livro(nome, genero, autor_nome, ano, valor))
-        print(f"Livro {nome} foi Adicionado com sucesso")
-        
+        _biblioteca_livros.append(Livro(nome, genero, autor_nome, ano, valor))
+        print(f"Livro {nome} foi adicionado com sucesso")
+        return True
 
-class listar_livros():
-    def __init__(self, biblioteca_livros):
-        self.livros = biblioteca_livros
-        self._index = 0
-    
-    def __iter__(self):
-        return self
+# Criando um gerador para listar os livros
+@log_operacao
+def listar_livros(biblioteca_livros):
+    for livro in biblioteca_livros:
+        yield f"""\
         
-    def __next__(self):
-        try:
-            livro = self.livros[self._index]
-            return f"""\
-            
-            Livro:\t{livro.nome}
-            genero:\t{livro.genero}
-            Autor:\t{livro.autor_nome}
-            Ano:\t{livro.ano}
-            Valor:\tR$ {livro.valor}
-            {"-" * 20}"""
-        except IndexError:
-            raise StopIteration
-        finally:
-            self._index += 1
+        Livro:	{livro.nome}
+        Gênero:	{livro.genero}
+        Autor:	{livro.autor_nome}
+        Ano:	{livro.ano}
+        Valor:	R$ {livro.valor}
+        {"-" * 20}"""
 
 def show_menu():
     print(f"""
@@ -66,15 +64,15 @@ def menu_livraria():
         show_menu()
         input_menu = input("Selecione uma opção: ")
         if input_menu == "1": # Adicionar livros
-            print("Informe os dados nescessários para adicionar o livro")
-            nome = input("nome: ")
-            genero = input("genero: ")
-            autor_nome = input("autor_nome: ")
-            ano = input("ano: ")
-            valor = input("valor: R$")
+            print("Informe os dados necessários para adicionar o livro")
+            nome = input("Nome: ")
+            genero = input("Gênero: ")
+            autor_nome = input("Autor: ")
+            ano = input("Ano: ")
+            valor = input("Valor: R$")
             adicionar_livros(nome, genero, autor_nome, ano, valor)
 
-        elif input_menu == "2": # Listar livro
+        elif input_menu == "2": # Listar livros
             for livro in listar_livros(_biblioteca_livros):
                 print(livro)
         elif input_menu == "3": # Buscar livro por nome
